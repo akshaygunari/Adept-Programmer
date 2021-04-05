6e9ecc8c1fb22cb8d3b45192684a0e9601441274
akshaygunari <akshaygunari@gmail.com>
2021-04-05 13:47:35 +0530

    May-2021

---
+++ Codechef Long Challenges/2020/April-2021/valid_pair.cpp
@@ -0,0 +1,24 @@
+/*
+Akshaykumar Gunari _AK_
+05-05-2021
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int A, B, C;
+	cin>>A>>B>>C;
+	if(A==B || A==C || B==C)
+	{
+	    cout<<"YES";
+	}
+	else
+	{
+	    cout<<"NO";
+	}
+	return 0;
+}
